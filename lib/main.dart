c4bb20ff908132c5699bebfbbf545507cee8f816
Amni Humaira <amnihumaira007@gmail.com>
2026-04-23 23:52:43 +0800

    update code section 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'package:my_app/gradient_container.dart';
+
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+  runApp(
+    const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Hello World!',
-              ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
-          ),
+        body:  GradientContainer(
+        Color.fromARGB(255, 30, 16, 131),
+        Color.fromARGB(255, 72, 48, 250),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
+
